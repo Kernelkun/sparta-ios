b441b8a6524532d4ef6742e2a302fc5cbb848466
Yaroslav Babalich <yaroslav.babalich@gmail.com>
2022-05-19 11:18:29 +0300

    + fixed full screen button on live charts landscape mode

--- Sparta/Kit/Controllers/LiveCharts/LCPresenter/LCTradeChartPresenter.swift
+++ Sparta/Kit/Controllers/LiveCharts/LCPresenter/LCTradeChartPresenter.swift
@@ -85,8 +85,8 @@ class LCTradeChartPresenter {
 
             window?.addSubview(button) {
                 $0.size.equalTo(32)
-                $0.bottom.equalToSuperview().inset(edges.bottom + 44)
-                $0.right.equalToSuperview().inset(edges.right + 6)
+                $0.left.equalToSuperview().offset(edges.left + 6)
+                $0.top.equalToSuperview().inset(70)
             }
         }
     }
